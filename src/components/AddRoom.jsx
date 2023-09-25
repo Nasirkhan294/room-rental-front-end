b99e12672cbaff5074897fab64d27cc0f2068e59
Nasir <nasirbhatti.nk@gmail.com>
2023-09-25 02:39:24 -0700

    Add alert component

--- src/components/AddRoom.jsx
+++ src/components/AddRoom.jsx
@@ -10,6 +10,18 @@ import {
   Button,
   Textarea,
 } from "@material-tailwind/react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { ErrorMessage, Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import {
+  addRoom,
+  selectRoomMessage,
+  selectRoomStatus,
+} from "../redux/Room/roomSlice";
+import useToken from "../redux/Auth/useToken";
+import Alert from './Alert'
+import { Spinner } from "./Loader";
 
 const AddRoom = () => {
   return <div>AddRoom</div>;
---
+++ src/components/Alert.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Alert as MaterialAlert } from '@material-tailwind/react';
+import { useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setStatusIdle as setReservationStatus } from '../redux/Reservations/reservationsSlice';
+import { setStatusIdle as setRoomStatus } from '../redux/Room/roomSlice';
+import { setStatusIdle as setAuthStatus } from '../redux/Auth/authSlice';
+
+
+const Alert = ({ message }) => {
+    const [show, setShow] = useState(true);
+    const dispatch = useDispatch();
+    const { pathname } = useLocation();
+  
+    const setReservationStatusIdle = () => {
+      if (pathname === '/booking') dispatch(setReservationStatus());
+    };
+  
+    const setAuthStatusIdle = () => {
+      if (pathname === '/login') dispatch(setAuthStatus());
+    };
+  
+    const setRoomStatusIdle = () => {
+      if (pathname === '/add_room') dispatch(setRoomStatus());
+    };
+  
+    setTimeout(() => {
+      setShow(false);
+      setReservationStatusIdle();
+      setAuthStatusIdle();
+      setRoomStatusIdle();
+    }, 5000);
+  
+    const filterMessage = (msg) => {
+      const fm = [
+        ...new Set(
+          msg
+            .replace(/Validation failed:/g, '')
+            .replace(/can't be blank/g, 'is required')
+            .split(','),
+        ),
+      ].join(', ');
+      return fm;
+    };
+  
+    return (
+      <>
+        <MaterialAlert
+          className="mb-12 ml-2 smax:ml-0 z-50"
+          show={show}
+          color="red"
+          dismissible={{
+            onClose: () => {
+              setReservationStatusIdle();
+              setAuthStatusIdle();
+              setRoomStatusIdle();
+              setShow(false);
+            },
+          }}
+        >
+          {filterMessage(message) || ''}
+        </MaterialAlert>
+      </>
+    );
+  };
+  
+  Alert.propTypes = {
+    message: PropTypes.string.isRequired,
+  };  
+
+export default Alert
