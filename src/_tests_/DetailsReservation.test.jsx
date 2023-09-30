f4d67e548f58245f68c05868533e45633294aaef
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-09-30 17:22:25 +0530

    Add and run the test and passed in tests

---
+++ src/_tests_/DetailsReservation.test.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import store from '../redux/store';
+import '@testing-library/jest-dom/';
+import ReservationDetail from '../pages-home/DetailsReservation';
+
+describe('Reservation Detail component', () => {
+    test('Should render the proper Reservation Detail component', () => {
+      const reservationDetail = render(
+        <React.StrictMode>
+          <Provider store={store}>
+            <Router>
+              <ReservationDetail title checkinDate checkoutDate hotel />
+            </Router>
+          </Provider>
+          ,
+        </React.StrictMode>,
+      );
+      expect(reservationDetail).toMatchSnapshot();
+    });
+  });
