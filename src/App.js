143deec4c221a1349a241c5e3e7155bf5051f5ca
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-10-05 18:12:21 +0530

    Update App.js file to fix error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import useToken from './redux/Auth/useToken';
+import { unsetAuthToken } from './api/api';
 import AppRouter from './routers';
 
 function App() {
+  if (useToken) unsetAuthToken();
   return (
     <div className="App">
       <AppRouter />
